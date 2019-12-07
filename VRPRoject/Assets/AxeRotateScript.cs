d701b32b0325ff644f5a9252f3f96bf2502415a6
Bendakh <34164678+Bendakh@users.noreply.github.com>
2019-12-07 21:45:08 +0100

    Init
    Initial Commit?

---
+++ VRPRoject/Assets/AxeRotateScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxeRotateScript : MonoBehaviour
+{
+    public bool x;
+    public bool y;
+    public bool z;
+
+    public int speed;
+
+    private Rigidbody2D rb2d;
+    private Vector3 rotationAxis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(x)
+        {
+            rotationAxis.x = 1;
+        }
+        else
+        {
+            rotationAxis.x = 0;
+        }
+
+        if (y)
+        {
+            rotationAxis.y = 1;
+        }
+        else
+        {
+            rotationAxis.y = 0;
+        }
+
+        if (z)
+        {
+            rotationAxis.z = 1;
+        }
+        else
+        {
+            rotationAxis.z = 0;
+        }
+        transform.Rotate(new Vector3(rotationAxis.x * speed, rotationAxis.y * speed, rotationAxis.z * speed));
+    }
+}
---
+++ VRPRoject/Assets/ThrowScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class ThrowScript : MonoBehaviour
+{
+    
+
+    [SerializeField]
+    private GameObject shurikenPrefab;
+    [SerializeField]
+    private Transform instantiatePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject shu = Instantiate(shurikenPrefab, instantiatePosition.position, Quaternion.identity);
+            shu.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 20);
+        }
+    }
+}
